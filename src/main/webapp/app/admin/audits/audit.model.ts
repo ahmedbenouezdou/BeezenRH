ceccfda01928b5971b22072ef879f79418cb2800
khadijasejjil <sejjil.khadija@gmail.com>
2018-09-02 19:35:16 +0200

    khadija

--- src/main/webapp/app/admin/audits/audit.model.ts
+++
@@ -1,5 +0,0 @@
-import { AuditData } from 'app/admin/audits/audit-data.model';
-
-export class Audit {
-    constructor(public data: AuditData, public principal: string, public timestamp: string, public type: string) {}
-}
--- src/main/webapp/app/layouts/navbar/navbar.route.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-import { NavbarComponent } from 'app/layouts/navbar/navbar.component';
-
-export const navbarRoute: Route = {
-    path: '',
-    component: NavbarComponent,
-    outlet: 'navbar'
-};
