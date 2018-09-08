819fab19cab39cfeff34ffb2e1792c28a3539b3a
khadijasejjil <sejjil.khadija@gmail.com>
2018-09-08 23:37:53 +0200

    khadija

---
+++ src/test/java/com/beezen/beezenrh/BeezenrhApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beezen.beezenrh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeezenrhApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
